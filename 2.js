c9c5f00b2b08fc43b17f28077acae72db3ed5714
Mohd Talib <103596639+talibbb13@users.noreply.github.com>
2024-04-17 17:11:14 +0530

    Initial Commit

---
+++ 2.js
@@ -0,0 +1,14 @@
+gsap.to("#page1 h1", {
+  transform: "translateX(-100%)",
+  fontWeight: "100",
+  scrollTrigger: {
+    trigger: "page1",
+    scroller: "body",
+    markers: true,
+    start: "top 0",
+    end: "top -200%",
+    scrub: 5,
+    pin: true
+  }
+})
+
